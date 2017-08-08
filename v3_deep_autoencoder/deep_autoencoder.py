3b83b9673ad1bc9ad1a1d665b5f9c0ef993542c3
Son <leson.phung@gmail.com>
2017-08-08 21:05:22 +1000

    Refactor deep autoencoder a bit

--- v3_deep_autoencoder/deep_autoencoder.py
+++ v3_deep_autoencoder/deep_autoencoder.py
@@ -3,77 +3,15 @@ import tensorflow as tf
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import Imputer
 
-from infants import deaths
-from infants import births
-from infants import X_COLUMNS
+from infants import X_all
+from infants import y_all
 
 import logging
 import warnings
 
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-X1, y1 = deaths[X_COLUMNS], pd.Series([1] * 24174)
-X2, y2 = births[X_COLUMNS], pd.Series([0] * 24175)
-
-
-logging.debug("Normalizing X1, X2")
-def mf_to_number(value):
-    if value == 'M':
-        return 0
-    if value == 'F':
-        return 1
-
-X1['sex'] = X1['sex'].apply(mf_to_number)
-X2['sex'] = X2['sex'].apply(mf_to_number)
-
-def yn_to_number(value):
-    if value == 'N':
-        return 0
-    if value == 'Y':
-        return 1
-
-yn_columns = [
-    'cig_rec', 'rf_diab', 'rf_gest', 'rf_phyp', 'rf_ghyp', 'rf_eclam', 'rf_ppterm', 'rf_ppoutc',
-    'rf_cesar',
-    'op_cerv', 'op_tocol', 'op_ecvs', 'op_ecvf',
-    'on_ruptr', 'on_abrup', 'on_prolg', 'ld_induct', 'ld_augment', 'ld_nvrtx', 'ld_steroids',
-    'ld_antibio', 'ld_chorio', 'ld_mecon', 'ld_fintol', 'ld_anesth',
-    'md_attfor', 'md_attvac',
-    'ab_vent', 'ab_vent6', 'ab_nicu', 'ab_surfac', 'ab_antibio',
-    'ca_anen', 'ca_menin', 'ca_heart', 'ca_hernia', 'ca_ompha', 'ca_gastro', 'ca_limb',
-    'ca_cleftlp', 'ca_cleft', 'ca_downs', 'ca_chrom', 'ca_hypos',
-]
-
-for yn_column in yn_columns:
-    X1[yn_column] = X1[yn_column].apply(yn_to_number)
-    X2[yn_column] = X2[yn_column].apply(yn_to_number)
-
-def xyn_to_number(value):
-    if value == 'N':
-        return 0
-    if value == 'Y':
-        return 1
-    if value == 'X':
-        return 2
-
-X1['md_trial'] = X1['md_trial'].apply(xyn_to_number)
-X2['md_trial'] = X2['md_trial'].apply(xyn_to_number)
-logging.debug("Normalized X1, X2")
-
-
-X_all, y_all = pd.concat([X1, X2]), pd.concat([y1, y2])
-
-
-logging.debug("Imputing & scaling X1, X2")
-imputer = Imputer(missing_values = 'NaN')
-X_all = imputer.fit_transform(X_all)
-min_max_scaler = MinMaxScaler()
-X_all = min_max_scaler.fit_transform(X_all)
-logging.debug("Imputed & scaled X1, X2")
 
 
 X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.30)
@@ -177,10 +115,10 @@ loss_curve_file = open('loss_curve.csv', 'w')
 
 # Launch the graph
 with tf.Session() as sess:
-    x_train_size = int(X_train.size/n_input)
-    total_batch = int(x_train_size/batch_size)
+    x_train_size = X_train.size // n_input
+    total_batch = x_train_size // batch_size
     logging.debug("Running session total_batch={:d}, batch_size={:d}, x train size={:d}".format(
-        total_batch, batch_size, x_train_size
+        total_batch, batch_size, x_train_size,
     ))
     sess.run(init)
 
