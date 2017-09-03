6985d0422ee2f607b8bff4c033a7eefbf202b937
Son <leson.phung@gmail.com>
2017-09-03 14:17:54 +1000

    Add mean value

--- v4_denoising_autoencoder/infants_scaler.rb
+++ v4_denoising_autoencoder/infants_scaler.rb
@@ -4,6 +4,25 @@ require 'finishing_moves'
 
 scaled_values = {}
 
+CSV.foreach('infants_columns_hash.csv') do |row|
+  column_name, values_hash = row
+  values_hash = JSON.parse(values_hash)
+  unique_values = values_hash.keys
+
+  values = unique_values.select { |v| v != '' }
+
+  if values.size > 0 && values[0].numeric?
+    values = values.map(&:to_f)
+    values_count = values_hash.values.reduce(&:+)
+    values_sum = values_hash.map do |key, value|
+      key.to_f * value
+    end.reduce(&:+)
+
+    scaled_values[column_name] ||= {}
+    scaled_values[column_name][:mean] = values_sum / values_count
+  end
+end
+
 CSV.foreach('infants_columns.csv') do |row|
   column_name, unique_values = row
   unique_values = JSON.parse(unique_values)
@@ -12,13 +31,10 @@ CSV.foreach('infants_columns.csv') do |row|
 
   if values.size > 0 && values[0].numeric?
     unique_values = unique_values.map(&:to_f)
-    min = unique_values.min
-    max = unique_values.max
 
-    scaled_values[column_name] = {
-      min: min,
-      max: max,
-    }
+    scaled_values[column_name] ||= {}
+    scaled_values[column_name][:min] = unique_values.min
+    scaled_values[column_name][:max] = unique_values.max
   end
 end
 
